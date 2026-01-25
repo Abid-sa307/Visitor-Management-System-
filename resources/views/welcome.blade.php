05e0f81d904d03a7832e07dd482552f0acb9f91e
Abid-sa307 <saiyedabid2803@gmail.com>
2026-01-25 21:05:48 +0530

    third push

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -642,7 +642,6 @@ $schema = [
                 <div class="col-lg-6 text-white">
                     <h1 class="display-4 fw-bold animate-fadeIn mb-4">Visitor Management System for All Workplaces
                         Worldwide</h1>
-                    <h1 class="display-4 fw-bold animate-fadeIn mb-4">testing text</h1>                    
                     <!-- Mobile Image - Only visible on mobile -->
                     <div class="mobile-image-container mb-4">
                         <img src="/images/visitor-management-system-main-img1.jpeg" alt="Visitor Management System"
