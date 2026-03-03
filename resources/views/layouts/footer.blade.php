8c14fbe74275d6e97edf7a51c80eaec7c063461a
Abid-sa307 <saiyedabid2803@gmail.com>
2026-03-03 13:41:05 +0530

    new changes

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -136,6 +136,3 @@
     </div>
   </div>
 </footer>
-
-<!-- Bootstrap JS -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
--- resources/views/layouts/website.blade.php
+++ resources/views/layouts/website.blade.php
@@ -61,8 +61,8 @@
     @include('layouts.footer')
 
     {{-- JS libs --}}
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+    <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script defer src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
 
     {{-- ✅ IMPORTANT: component/page pushed scripts --}}
     @stack('scripts')
