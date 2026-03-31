c9124e8c383df0575a56a7add1056e5f11859914
Abid-sa307 <saiyedabid2803@gmail.com>
2026-03-31 14:15:12 +0530

    blog updation

---
+++ config/sanity.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'project_id' => env('SANITY_PROJECT_ID', '1bthezjc'),
+    'dataset' => env('SANITY_DATASET', 'production'),
+    'api_version' => env('SANITY_API_VERSION', '2023-10-01'),
+];
